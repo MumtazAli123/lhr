d91e449851d3188eae8135e9fea85017e5cae348
Mumtaz <kad.dadu@gmail.com>
2022-11-05 08:52:47 +0500

    updated

--- lib/signup.dart
+++ lib/signup.dart
@@ -5,9 +5,23 @@ class Signup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.cyan,
-      body: Text('Ali'),
-    );
+    return Scaffold(
+        backgroundColor: Colors.cyan,
+        body: SafeArea(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Signup',
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.w900),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
